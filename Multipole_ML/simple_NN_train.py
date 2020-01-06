4e1d7c6c6f06ad58dc327adb13a36f5b863e8a7c
eisenhart-andrew <eisenhaw@mail.uc.edu>
2020-01-06 14:01:39 -0500

    Update simple_NN_train.py

--- Multipole_ML/simple_NN_train.py
+++ Multipole_ML/simple_NN_train.py
@@ -1,11 +1,25 @@
-
+from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-tf.set_random_seed(1)
-np.random.seed(1)
+df = pd.read_csv('mol1.csv')
 
+df['atom id'] = pd.Categorical(df['atom id'])
+df['atom id'] = df.thal.cat.codes
+
+target = df.pop('target')
+
+dataset = tf.data.Dataset.from_tensor_slices((df.values, target.values))
+
+for feat, targ in dataset.take(5):
+  print ('Features: {}, Target: {}'.format(feat, targ))
+
+tf.constant(df['atom id']
+
+train_dataset = dataset.shuffle(len(df)).batch(1)            
+            
 tf_x = tf.placeholder(tf.float32, x.shape)     # input x
 tf_y = tf.placeholder(tf.float32, y.shape)     # input y
 
