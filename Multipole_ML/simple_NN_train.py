6a73e2a6259f0cafad126e3da56dfa345689c595
eisenhart-andrew <eisenhaw@mail.uc.edu>
2020-01-06 10:41:00 -0500

    Update simple_NN_train.py

--- Multipole_ML/simple_NN_train.py
+++ Multipole_ML/simple_NN_train.py
@@ -1,26 +1,10 @@
-"""
-Know more, visit my Python tutorial page: https://morvanzhou.github.io/tutorials/
-My Youtube Channel: https://www.youtube.com/user/MorvanZhou
-Dependencies:
-tensorflow: 1.1.0
-matplotlib
-numpy
-"""
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
 tf.set_random_seed(1)
 np.random.seed(1)
-
-# fake data
-x = np.linspace(-1, 1, 100)[:, np.newaxis]          # shape (100, 1)
-noise = np.random.normal(0, 0.1, size=x.shape)
-y = np.power(x, 2) + noise                          # shape (100, 1) + some noise
-
-# plot data
-plt.scatter(x, y)
-plt.show()
 
 tf_x = tf.placeholder(tf.float32, x.shape)     # input x
 tf_y = tf.placeholder(tf.float32, y.shape)     # input y
